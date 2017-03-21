8d5e9feab1c8941358efd26611f2188d91167805
Blesa Patricia <blesapati@gmail.com>
2017-03-21 18:06:03 +0100

    add conditions-exercises 18-19

---
+++ week-02/day-02/ex-condition/odd-even.py
@@ -0,0 +1,7 @@
+print("hello, adj meg egy szamot ")
+userNum = int(input())
+
+if (userNum % 2) == 0:
+    print("a te szamod paros, juhhee")
+else:
+    print("a te szamod egyszeruen paratlan!")
---
+++ week-02/day-02/ex-condition/one-two-a-lot.py
@@ -0,0 +1,8 @@
+print("hello, adj meg egy szamot ")
+userNum = int(input())
+
+if userNum == 0:
+    print("Nem eleg")
+if userNum == 1 or userNum == 2:
+    print(str(userNum))
+else: print("Tul sok")
