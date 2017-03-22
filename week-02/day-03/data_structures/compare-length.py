c765211a14fbbd6f8779eaf2e17f817b85c9b194
Blesa Patricia <blesapati@gmail.com>
2017-03-22 11:48:51 +0100

    add compare-length exercise

---
+++ week-02/day-03/data_structures/compare-length.py
@@ -0,0 +1,14 @@
+p1 = [1, 2, 3]
+p2 = [5, 6]
+
+if len(p1) < len(p2):
+    print("`p2` has more elements than `p1`")
+else:
+    print("`p1` has more elements than `p2`")
+
+
+# - Create a variable named `p1`
+#   with the following content: `[1, 2, 3]`
+# - Create a variable named `p2`
+#   with the following content: `[4, 5]`
+# - Print if `p2` has more elements than `p1`
