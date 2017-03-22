74b622dcd2a6b937aba12909f8490c93d06375a6
Blesa Patricia <blesapati@gmail.com>
2017-03-22 12:58:39 +0100

    add append-a exercise, maybe need modification

---
+++ week-02/day-03/functions/append-a.py
@@ -0,0 +1,11 @@
+am = "kuty"
+
+def appendA(wor = "lil"):
+    print(wor + "a")
+
+appendA(am)
+appendA()
+# - Create a variable named `am` and assign the value `kuty` to it
+# - Write a function called `appendA` that gets a string as an input
+#   and appends an 'a' character to its end
+# - Print the result of `appendA(am)`
