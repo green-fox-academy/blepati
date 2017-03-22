0faa374b274a34c2de03012e74f3177978b81176
Blesa Patricia <blesapati@gmail.com>
2017-03-22 15:32:31 +0100

    add change-element exercise with built-in dta structure

---
+++ week-02/day-03/data_structures/change-element.py
@@ -0,0 +1,12 @@
+s = [1, 2, 3, 8, 5, 6]
+
+s.pop(3)
+s.append(4)
+s.sort()
+print(s[3])
+
+
+# - Create an array variable named `s`
+#   with the following content: `[1, 2, 3, 8, 5, 6]`
+# - Change the 8 to 4
+# - Print the fourth element
