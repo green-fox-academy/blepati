dcb5567531715bdb577a3b9b93a45eaad91ef032
Blesa Patricia <blesapati@gmail.com>
2017-03-22 16:37:31 +0100

    add modified append-a exercise with array

---
+++ week-02/day-03/data_structures/append-a-array.py
@@ -0,0 +1,9 @@
+nimals = ["kuty", "macsk", "cic", "ar"]
+a = "a"
+
+for i in range(len(nimals)):
+    print(nimals[i] + a)
+
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macs", "cic"]`
+# - Add all elements an `"a"` at the end
