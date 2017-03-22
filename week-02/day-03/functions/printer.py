9171833f538334ae4171651b8ca74781b7de6c83
Blesa Patricia <blesapati@gmail.com>
2017-03-22 14:19:35 +0100

    move factorio file from data folder to functions

---
+++ week-02/day-03/functions/printer.py
@@ -0,0 +1,8 @@
+def printer(prWord = "hello"):
+    print(prWord)
+
+printer("bello")
+
+# - Create a function called `printer`
+#   which prints the input parameters
+#   (can have multiple number of arguments)
