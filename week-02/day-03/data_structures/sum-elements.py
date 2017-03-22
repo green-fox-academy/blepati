03c0153a3a04d7aa58904bbcbbfeb1992b032df7
Blesa Patricia <blesapati@gmail.com>
2017-03-22 13:33:06 +0100

    add sum-element excersise

---
+++ week-02/day-03/data_structures/sum-elements.py
@@ -0,0 +1,10 @@
+r = (54, 23, 66, 12)
+
+def summa(num1 = r[1], num2= r[2]):
+    print(num1 + num2)
+
+summa()
+
+# - Create a variable named `r`
+#   with the following content: `[54, 23, 66, 12]`
+# - Print the sum of the second and the third element
