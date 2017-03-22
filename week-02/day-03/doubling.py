37d2735d38a571026736c5bc59730d30896d39c9
Blesa Patricia <blesapati@gmail.com>
2017-03-22 11:15:40 +0100

    add day-03 folder and doubling.py exercise

---
+++ week-02/day-03/doubling.py
@@ -0,0 +1,16 @@
+ak = 123
+
+def doubling1():
+    print(ak * 2)
+
+doubling1()
+
+def doubling2():
+    print((str(ak) + " ") * 2)
+
+doubling2()
+
+
+# - Create a variable named `ak` and assign the value `123` to it
+# - Create a function called `doubling` that doubles it's input parameter
+# - Print the result of `doubling(ak)`
