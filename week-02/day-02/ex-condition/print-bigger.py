bf702d9e017ad1e811247d2d174afa1efbb01b13
Blesa Patricia <blesapati@gmail.com>
2017-03-23 11:48:53 +0100

    finish print bigger exercise

--- week-02/day-02/ex-condition/print-bigger.py
+++ week-02/day-02/ex-condition/print-bigger.py
@@ -1,4 +1,9 @@
 # Write a program that asks for two numbers and prints the bigger one
 print("hello, adj meg ket szamot ")
 userNum1 = int(input())
-userNum2 =
+userNum2 =int(input())
+
+if userNum1 > userNum2:
+    print("a nagyobb szam a: " + str(userNum1))
+else:
+    print("a nagyobb szam a: " + str(userNum2))
