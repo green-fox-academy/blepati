afdaaedd83fe05917aac218648571d1c55c3821e
Blesa Patricia <blesapati@gmail.com>
2017-03-28 16:40:22 +0200

    fix divide-by-0.py

--- week-03/day-02/exceptions/divide-by-0.py
+++ week-03/day-02/exceptions/divide-by-0.py
@@ -1,13 +1,13 @@
-num = input("Please, write in a number: ")
-divider = 10
 
-try:
-    z = divider / int(num)
-    print(z)
-except ZeroDivisionError:
-    print("fail")
+def kacsa(num):
+    divider = 10
+    try:
+        z = divider / num
+        print(z)
+    except ZeroDivisionError:
+        print("fail")
 
-
+kacsa(0)
 # create a function that takes a number,
 # divides ten with it,
 # and prints the result.
