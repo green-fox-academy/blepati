85a02cabe721f03e935464cf99907ced0c71b333
Blesa Patricia <blesapati@gmail.com>
2017-03-28 18:01:54 +0200

    finish duplicated exercise and fix divide-by-0

--- week-03/day-02/doubled/duplicated.py
+++ week-03/day-02/doubled/duplicated.py
@@ -1,7 +1,10 @@
 duplicated = open("duplicated-chars.txt")
-#print(duplicated.readline())
+#print(duplicated.read())
 letters = duplicated.read()
+ll = len(letters)
+
+print(letters[0:ll:2])
 
 
-
+#minden paratlan betut printeljen ki (valami%2 == 1)
 #duplicated.close()
