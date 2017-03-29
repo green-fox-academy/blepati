8b9a686353f309bbc1159402b05c8d76322bc601
Blesa Patricia <blesapati@gmail.com>
2017-03-29 13:55:14 +0200

    add purple-steps exercise

--- week-03/day-03/canvas-ex/purple-steps.py
+++ week-03/day-03/canvas-ex/purple-steps.py
@@ -4,15 +4,16 @@ root = Tk()
 
 canvas = Canvas(root, width='300', height='300')
 canvas.pack()
-rainbowcolor = ["red", "orange", "yellow", "green", "blue", "purple"]
-size = 300
 
-def center(size, color):
-    for color in rainbowcolor:
-        size -= 30
-        canvas.create_rectangle(150 - (size/2), 150 - (size/2), 150 + (size/2), 150 + (size/2), fill=color)
+def stair(size, many):
+    x = 0
+    y = 0
+    for i in range(many):
+        canvas.create_rectangle(x, y,  x + size, y + size, fill="purple")
+        x += size
+        y += size
 
-center(size, rainbowcolor)
+stair(10, 12)
 # create a square drawing function that takes 2 parameters:
 # the square size, and the fill color,
 # and draws a square of that size and color to the center of the canvas.
