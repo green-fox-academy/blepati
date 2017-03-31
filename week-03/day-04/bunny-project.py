1de30830e6b9519e532688b604007352e2a245d6
Blesa Patricia <blesapati@gmail.com>
2017-03-31 15:08:45 +0200

    add static bunny with proper values

--- week-03/day-04/bunny-project.py
+++ week-03/day-04/bunny-project.py
@@ -5,11 +5,16 @@ root = Tk()
 canvas = Canvas(root, width='600', height='600')
 canvas.pack()
 
-def bunny_draw(color):
-    ear1 = canvas.create_oval(235, 140, 275, 280, outline=color, fill=color)
-    ear2 = canvas.create_oval(335, 140, 375, 280, outline=color, fill=color)
-    body = canvas.create_oval(175, 250, 425, 550, outline=color, fill=color)
-    ground = canvas.create_rectangle(0, 500, 600, 600, outline="#5c3c2d", fill="#5c3c2d")
+#def bunny_draw(color):
+    #ear1 = canvas.create_oval(235, 140, 275, 280, outline=color, fill=color)
+    #ear2 = canvas.create_oval(335, 140, 375, 280, outline=color, fill=color)
+    #body = canvas.create_oval(175, 250, 425, 550, outline=color, fill=color)
+    #ground = canvas.create_rectangle(0, 500, 600, 600, outline="#5c3c2d", fill="#5c3c2d")
 
-bunny_draw("green")
+def bunny_draw(main, color):
+    ear1 = canvas.create_oval(main/2+main/4,main/2-main/4, main, main, outline=color, fill=color)
+    ear1 = canvas.create_oval(main/2+main/2,main/2-main/4, main+main/4, main, outline=color, fill=color)
+    body = canvas.create_oval(main/2, main/2, main+main/2, 2*main, outline=color, fill=color)
+    ground = canvas.create_rectangle(0, main+main/2, main*2, main*2, outline="#5c3c2d", fill="#5c3c2d")
+bunny_draw(300, "#20d1ac")
 root.mainloop()
