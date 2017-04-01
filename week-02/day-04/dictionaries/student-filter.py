5536955467e384527b59f0e16ed45bb4bea1bbd4
Blesa Patricia <blesapati@gmail.com>
2017-04-01 18:09:54 +0200

    add student-filter exercise

--- week-02/day-04/dictionaries/student-filter.py
+++ week-02/day-04/dictionaries/student-filter.py
@@ -5,8 +5,20 @@ students = [
         {'name': 'Zsombor', 'age': 12, 'candies': 5}
 ]
 
+def who_have_most(s):
+    for student in s:
+        if student['candies'] > 4:
+            print(student['name'])
 
+who_have_most(students)
 
+def candy_average(s):
+    candy = 0
+    for student in s:
+        candy += student['candies']
+    return candy / len(s)
+
+print(candy_average(students))
 # create a function that takes a list of students and prints:
 # - Who has got more candies than 4 candies
 
