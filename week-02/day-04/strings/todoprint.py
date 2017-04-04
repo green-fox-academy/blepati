65ba8fd6480a203c9d9048a8c787531da2836265
Blesa Patricia <blesapati@gmail.com>
2017-04-04 13:19:10 +0200

    add todo exercise file without solution, need to finish

---
+++ week-02/day-04/strings/todoprint.py
@@ -0,0 +1,16 @@
+# Add "My todo:" to the beginning of the todoText
+# Add " - Download games" to the end of the todoText
+# Add " - Diablo" to the end of the todoText but with indention
+
+# Expected outpt:
+
+# My todo:
+#  - Buy milk
+#  - Download games
+#      - Diablo
+
+todoText = [" - Buy milk\n"]
+todoText.append(" - Download games")
+#todoText.append()
+
+print(str(todoText))
