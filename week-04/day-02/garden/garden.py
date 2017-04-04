c9c4a5ac4b66156d1992860d93cc239fdd9d0e41
Blesa Patricia <blesapati@gmail.com>
2017-04-04 16:29:00 +0200

    add garden folder and 3 files to work on garden exercise

---
+++ week-04/day-02/garden/garden.py
@@ -0,0 +1,13 @@
+import Tree from tree
+import Flower from flower
+
+class Garden(Flower, Tree):
+    def __init__(self, flowers = 0, trees = 0):
+        self.flowers = []
+        self.trees = []
+
+    def add_flower(self, flower):
+        self.flower.append(flowers)
+
+    def add_tree(self, tree):
+        self.tree.append(trees)
