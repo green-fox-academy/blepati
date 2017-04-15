90ac31549016a0298ae7368e9485c37a0436e6ee
Blesa Patricia <blesapati@gmail.com>
2017-04-15 15:29:57 +0200

    feat(garden) fix need_water function and begin to build watering fuction too

--- week-04/day-02/garden/garden.py
+++ week-04/day-02/garden/garden.py
@@ -5,23 +5,34 @@ class Garden(object):
     def __init__(self, flowers = 0, trees = 0):
         self.flowers_inside = []
         self.trees_inside = []
+        self.dry_plant = 0
 
-    def print_statuses(self):
-        for flower in self.flowers_inside:
-            flower.need_water()
-        for tree in self.trees_inside:
-            tree.need_water()
-            
     def add_flower(self, flower):
         self.flowers_inside.append(flower)
 
     def add_tree(self, tree):
         self.trees_inside.append(tree)
 
-    #def dry_plants(self, ):
+    def print_statuses(self):
+        for flower in self.flowers_inside:
+            if flower.need_water():
+                self.dry_plant += 1
+                print("The " + flower.color + " Flower needs water")
+            else:
+                print("The " + flower.color + " Flower does not need water")
+        for tree in self.trees_inside:
+            if tree.need_water():
+                self.dry_plant += 1
+                print("The " + tree.color + " Tree needs water")
+            else:
+                print("The " + tree.color + " Tree does not need water")
+
+    def calculate_amount(self):
+        pass
+
 
 garden = Garden()
-flower1 = Flower("yellow", 4)
+flower1 = Flower("yellow", 7)
 flower2 = Flower("red", 2)
 garden.add_flower(flower1)
 garden.add_flower(flower2)
@@ -30,3 +41,4 @@ tree2 = Tree("brown", 5)
 garden.add_tree(tree1)
 garden.add_tree(tree2)
 garden.print_statuses()
+print(garden.dry_plant)
--- week-04/day-02/garden/plants.py
+++ week-04/day-02/garden/plants.py
@@ -4,10 +4,7 @@ class Flower(object):
         self.amount = amount
 
     def need_water(self):
-        if self.amount <= 5:
-            print("The " + self.color + " Flower needs water")
-        else:
-            print("The " + self.color + " Flower does not need water")
+        return self.amount <= 5
 
     def watering(self, amount):
         self.amount += amount * 0.75
@@ -18,10 +15,7 @@ class Tree(Flower):
         super().__init__(color, amount)
 
     def need_water(self):
-        if self.amount <= 10:
-            print("The " + self.color + " Tree needs water")
-        else:
-            print("The " + self.color + " Tree does not need water")
+        return self.amount <= 10
 
     def watering(self, amount):
         self.amount += amount * 0.40
