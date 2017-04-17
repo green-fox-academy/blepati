f564abf18432f45760cd000c0c8aea1be550cbca
Blesa Patricia <blesapati@gmail.com>
2017-04-17 23:03:57 +0200

    feat(aircraft) create F16 and F30 classes and create a carrier file

--- week-04/day-03/aircraft/aircraft.py
+++ week-04/day-03/aircraft/aircraft.py
@@ -1,5 +1,6 @@
 class Aircraft(object):
-    def __init__(self, max_ammo = 0, base_damage = 0):
+    def __init__(self, aircraft_type = " ", max_ammo = 0, base_damage = 0):
+        self.aircraft_type = aircraft_type
         self.max_ammo = max_ammo
         self.base_damage = base_damage
         self.ammo = 0
@@ -18,6 +19,23 @@ class Aircraft(object):
             remaining_bullet = bullet_amount - self.max_ammo
             return remaining_bullet
 
-aircraft = Aircraft(12, 50)
+    def get_type(self):
+        print(str(self.aircraft_type))
+
+    def get_status(self):
+        print("Type: " + str(self.aircraft_type) + ", Ammo: " + str(self.ammo) + ", Base Damage: " + str(self.base_damage) + ", All Damage: " + str(self.fight()))
+
+class F16(Aircraft):
+    def __init__(self):
+        super().__init__("F16", 8, 30)
+
+class F30(Aircraft):
+    def __init__(self):
+        super().__init__("F30", 12, 50)
+
+aircraft = F16()
 print(aircraft.refill(100))
 print(aircraft.fight())
+print(aircraft.refill(10))
+aircraft.get_type()
+aircraft.get_status()
---
+++ week-04/day-03/aircraft/carrier.py
@@ -0,0 +1,8 @@
+from aircraft import Aircraft
+
+class Carrier(object):
+    def __init__(self):
+        self.aircrafts_inside = []
+        self.health = 2300
+        self.bullet_amount = 2500
+        
