794b59f1ddd4e161254436496aa876c1e8559838
Blesa Patricia <blesapati@gmail.com>
2017-05-02 13:43:01 +0200

    add ex_03 with humpty

---
+++ week-07/day-02/ex_03.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Modify this program to console.log Humpty Dumpty riddle correctly
+var story = ['All the king\'s horses and all the king\'s men',
+             'Humpty Dumpty had a great fall.',
+             'Humpty Dumpty sat on a wall,',
+             'Couldn\'t put Humpty together again.']
+
+console.log(story[2] + '/n ' + story[1] + ' /n ' + story[0] + '/n ' + story[3])
