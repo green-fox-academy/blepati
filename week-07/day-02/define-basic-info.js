fac8a80ac3fa8c31217813e5732fa9e6c5aa0c83
Blesa Patricia <blesapati@gmail.com>
2017-05-02 14:54:22 +0200

    feat(define-basic-info) add 4 variables and log them out

---
+++ week-07/day-02/define-basic-info.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+var name = 'Patricia';
+var age = 29;
+var height = 1.61;
+var married = false;
+
+console.log (name +'\n' + age +'\n' + height + '\n' + married)
