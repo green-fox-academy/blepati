a6641e2c05832a7cec687a4e942ed7d00072b4e9
Blesa Patricia <blesapati@gmail.com>
2017-05-02 13:36:31 +0200

    add ex_02 and rename ex_01

---
+++ week-07/day-02/ex_02.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+var classNames = ['Robi', 'Nelli', 'Norbi']
+
+console.log('Hello ' + classNames[0])
+console.log('Hello ' + classNames[1])
+console.log('Hello ' + classNames[2])
