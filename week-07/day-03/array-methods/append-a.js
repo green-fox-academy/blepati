ef1bab8c4f170e54dcec2d77f3edc15b8b0231ef
Blesa Patricia <blesapati@gmail.com>
2017-05-03 13:47:29 +0200

    fat(append-a-array): append a letter with a built in function

---
+++ week-07/day-03/array-methods/append-a.js
@@ -0,0 +1,11 @@
+'use strict';
+// - Create a variable named `nimals`
+//   with the following content: `["kuty", "macs", "cic"]`
+// - Add all elements an `"a"` at the end
+// - try to use built in functions instead of loops
+
+var nimals = ["kuty", "macsk", "cic", "lib"];
+
+nimals.forEach(function(ani) {
+  console.log(ani + 'a')
+});
